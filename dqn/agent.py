298bb1b415f4fc116b2acff0fb5294ebb1247a2b
Mohamed Baioumy <mohamedbaioumy15@gmail.com>
2024-03-11 12:24:16 +0000

    Create agent.py

---
+++ dqn/agent.py
@@ -0,0 +1,22 @@
+import gymnasium as gym
+
+# Mkae cartpole from 'classical control' in gym
+env = gym.make("CartPole-v1")
+
+# Experience replay mem for training the dqn agent
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
